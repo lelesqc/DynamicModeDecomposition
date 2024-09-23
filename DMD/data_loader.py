2e6a475f59a633af2779762269604d662f163aea
Emanuele Nori <162329829+lelesqc@users.noreply.github.com>
2024-09-23 18:09:55 +0200

    Update data_loader.py
    Made some corrections, deleted apply_mask function

--- DMD/data_loader.py
+++ DMD/data_loader.py
@@ -1,34 +1,24 @@
-import torch as pt
+import numpy as np
 from flowtorch import DATASETS
 from flowtorch.data import FOAMDataloader, mask_box
 from config import DATASET_NAME, MASK_LOWER_BOUND, MASK_UPPER_BOUND
 
-def load_data(dataset_path=None):
-    try:
-        dataset = DATASETS[DATASET_NAME]
-        loader = FOAMDataloader(dataset)
-        times = loader.write_times
-        fields = loader.field_names
-        pts = loader.vertices[:, :2]    # Discard z-coordinate since simulation is only 2D
+def load_data():
+    dataset = DATASETS[DATASET_NAME]
+    loader = FOAMDataloader(dataset)
+    times = loader.write_times
+    fields = loader.field_names
+    pts = loader.vertices[:, :2]    # Discard z-coordinate since simulation is only 2D
+
+    if not times or np.isnan(times).any():
+        raise ValueError("Error: Time steps are either empty or contain NaN values.")
     
-        if not times or not fields or not pts.size(0):
-            raise ValueError("One or more required data items are empty. Verify that data has been loaded correctly.")
+    if not fields:
+        raise ValueError("Error: No fields available in the dataset.")
     
-        return loader, times, fields, pts
+    if pts.size == 0 or np.isnan(pts).any():
+        raise ValueError("Error: Vertices are either empty or contain NaN values.")
 
-    # Check for any other possible error occurring internally
-    except Exception as e:
-        raise RuntimeError(f"An error occurred while loading data: {str(e)}")
+    mask = mask_box(pts, lower=MASK_LOWER_BOUND, upper=MASK_UPPER_BOUND)
 
-
-def apply_mask(pts):
-    try:
-        mask = mask_box(pts, lower=MASK_LOWER_BOUND, upper=MASK_UPPER_BOUND)        
-        if mask.size(0) == 0:
-            raise ValueError("Mask is empty. Please check data loading.")
-            
-        return mask
-
-    # Check for any other possible error occurring internally
-    except Exception as e:
-        raise RuntimeError(f"An error occurred while applying the mask: {str(e)}")
+    return times, fields, pts, mask
