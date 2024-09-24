ad0114dc83d47bd22a22eb3078a75f7fa47a0ae6
Emanuele Nori <162329829+lelesqc@users.noreply.github.com>
2024-09-24 15:34:38 +0200

    Update data_loader.py
    Made some adjustments on the logic

--- DMD/data_loader.py
+++ DMD/data_loader.py
@@ -1,23 +1,30 @@
-import numpy as np
+from numpy import isnan
 from flowtorch import DATASETS
 from flowtorch.data import FOAMDataloader, mask_box
 from config import DATASET_NAME, MASK_LOWER_BOUND, MASK_UPPER_BOUND
 
-def load_data():
-    dataset = DATASETS[DATASET_NAME]
-    loader = FOAMDataloader(dataset)
+def load_data(loader=None):
+    if loader is None:
+        dataset = DATASETS[DATASET_NAME]
+        loader = FOAMDataloader(dataset)
+    
     times = loader.write_times
     fields = loader.field_names
-    pts = loader.vertices[:, :2]    # Discard z-coordinate since simulation is only 2D
+    pts = loader.vertices
 
-    if not times or np.isnan(times).any():
-        raise ValueError("Error: Time steps are either empty or contain NaN values.")
+    if pts.numel() == 0:
+        raise ValueError("Vertices are empty.")
+    
+    pts = pts[:, :2]    # Discard z-coordinate since simulation is only 2D
+
+    if not times:
+        raise ValueError("Time steps are empty.")
     
     if not fields:
-        raise ValueError("Error: No fields available in the dataset.")
-    
-    if pts.size == 0 or np.isnan(pts).any():
-        raise ValueError("Error: Vertices are either empty or contain NaN values.")
+        raise ValueError("No fields available in the dataset.")
+
+    if isnan(pts).any():
+        raise ValueError("One or more vertices value is NaN.") 
 
     mask = mask_box(pts, lower=MASK_LOWER_BOUND, upper=MASK_UPPER_BOUND)
 
