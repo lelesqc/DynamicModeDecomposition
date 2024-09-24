7be85cc16dfb57e6772249322a5716f2e3c4ae9a
Emanuele Nori <162329829+lelesqc@users.noreply.github.com>
2024-09-24 15:40:25 +0200

    Update test_data_loader.py
    Added fixtures, mocks and improved clarity

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -1,57 +1,111 @@
 import pytest
 import torch as pt
-from data_loader import load_data, apply_mask
+from unittest.mock import MagicMock
+from data_loader import load_data
+from flowtorch.data import FOAMDataloader
 
-def test_load_data_valid():
+# ------------------  FIXTURES  -----------------------
+
+# FOAMDataloader attributes (write_times, field_names, vertices) cannot be assigned directly.
+# We have to make use of mocking
+
+@pytest.fixture
+def empty_times_loader():
     """
-    Test that checks the correct loading of data.
-    
-    This test checks that the 'load_data' function successfully loads the dataset and
-    returns non-empty variables.
-    It asserts that:
-    
-    - 'times' is not empty 
-    - 'fields' is not empty
-    - 'pts' contains grid vertices
-    
-    These data are needed for the simulation, incorrect loading of at least one of them causes an error.
+    Fixture that simulates empty times using mocking.
     
     """
-    loader, times, fields, pts = load_data()
-    
-    assert len(times) > 0, "Times list is empty"
-    assert len(fields) > 0, "Fields dictionary is empty"
-    assert pts.size(0) > 0, "Tensor containing grid vertices is empty"
+    mock_loader = MagicMock()
+    mock_loader.write_times = [] 
+    mock_loader.field_names = {'4.0': 'velocity'}
+    mock_loader.vertices = pt.tensor([[1.0, 2.0], [3.0, 4.0]])
+    return mock_loader
 
-def test_load_data_incomplete():
-    """Test che verifica che venga sollevato un errore se una delle variabili di dati è vuota."""
+@pytest.fixture
+def empty_fields_loader():
+    """
+    Fixture that simulates empty fields using mocking.
     
-    with pytest.raises(ValueError, match="One or more required data items are empty"):
-        load_data(simulate_empty=True)
+    """
+    mock_loader = MagicMock()
+    mock_loader.write_times = ["1.0", "2.0", "3.0"]
+    mock_loader.field_names = {}
+    mock_loader.vertices = pt.tensor([[1.0, 2.0], [3.0, 4.0]])
+    return mock_loader
 
+@pytest.fixture
+def nan_pts_loader():
+    """
+    Fixture that simulates NaN pts using mocking.
+    
+    """
+    mock_loader = MagicMock()
+    mock_loader.write_times = ["1.0", "2.0", "3.0"]
+    mock_loader.field_names = {'4.0': 'velocity'}
+    mock_loader.vertices = pt.Tensor([[1.0, 2.0], [float('nan'), 1.0]])
+    return mock_loader
+
+@pytest.fixture
+def empty_pts_loader():
+    """
+    Fixture that simulates empty pts using mocking.
+    
+    """
+    mock_loader = MagicMock()
+    mock_loader.write_times = ["1.0", "2.0", "3.0"] 
+    mock_loader.field_names = {'4.0': 'velocity'}
+    mock_loader.vertices = pt.Tensor([])
+    return mock_loader
+
+
+# ---------------------  TESTS  ---------------------------
+
+def test_load_data_empty_times(empty_times_loader):
+    """
+    Test that verifies the correct raise of an error if times is empty.
+    It is paired with empty_times_loader fixture.
+
+    """
+    with pytest.raises(ValueError, match="Time steps are empty."):
+        load_data(loader=empty_times_loader)
+
+def test_load_data_empty_fields(empty_fields_loader):
+    """
+    Test that verifies the correct raise of an error if fields is empty.
+    It is paired with empty_fields_loader fixture.
+
+    """
+    with pytest.raises(ValueError, match="No fields available in the dataset."):
+        load_data(loader=empty_fields_loader)
+
+def test_load_data_nan_pts(nan_pts_loader):
+    """
+    Test that verifies the correct raise of an error if pts contains NaN values.
+    It is paired with nan_pts_loader fixture.
+
+    """
+    with pytest.raises(ValueError, match="One or more vertices value is NaN."):
+        load_data(loader=nan_pts_loader)
+
+def test_load_data_empty_pts(empty_pts_loader):
+    """
+    Test that verifies the correct raise of an error if pts is empty.
+    It is paired with empty_pts_loader fixture.
+
+    """
+    with pytest.raises(ValueError, match="Vertices are empty."):
+        load_data(loader=empty_pts_loader)
+    
 def test_apply_mask_valid():
-    """Test che verifica l'applicazione della maschera sui punti."""
-    _, _, _, pts = load_data()
+    """
+    Test that verifies mask is correctly implemented based on pts.
+    It must have the same size of the tensor on which it has been applied.
     
-    mask = apply_mask(pts)
+    """
+    _, _, pts, mask = load_data()    
+    
     assert mask.size(0) > 0, "Mask is empty"
-
-def test_apply_mask_empty():
-    """Test che verifica che venga sollevato un errore se la maschera è vuota."""
-    # Crea dei punti che sono fuori dai limiti della maschera
-    pts_outside_bounds = pt.tensor([[100, 100], [200, 200]], dtype=pt.float32)
-    
-    with pytest.raises(ValueError):
-        apply_mask(pts_outside_bounds)
-
-def test_apply_mask_invalid_input():
-    """Test che verifica che venga sollevato un errore se i punti non sono validi."""
-    # Crea un input non valido (ad esempio una lista invece di un tensore)
-    pts_invalid = [[0.5, 0.5], [1.0, 1.0]]  # Questo non è un tensore PyTorch
-
-    # Verifica che venga sollevata un'eccezione RuntimeError a causa di un input non valido
-    with pytest.raises(RuntimeError):
-        apply_mask(pts_invalid)
+    assert mask.size(0) == pts.size(0), "Mask has different size with respect to pts"
 
 
 
