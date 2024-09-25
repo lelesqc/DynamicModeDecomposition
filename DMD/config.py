7a5345cc1287fc360960ec993a95cd7e7144aa4e
Emanuele Nori <162329829+lelesqc@users.noreply.github.com>
2024-09-25 14:55:53 +0200

    Update config.py
    Removed MIN_TIME and DATASET_TYPE

--- DMD/config.py
+++ DMD/config.py
@@ -1,6 +1,4 @@
 DATASET_NAME = "of_cylinder2D_binary"
-DATASET_TYPE = "full"    # Digit "minimal" for the reduced dataset
 MASK_LOWER_BOUND = [.1, -1]
 MASK_UPPER_BOUND = [.75, 1]
-MIN_TIME = 4.0
 FIELD_NAME = "vorticity"
