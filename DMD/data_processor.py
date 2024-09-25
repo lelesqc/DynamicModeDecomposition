5e9fe67346d5ebe958a3fd45743a4ac41602d423
Emanuele Nori <162329829+lelesqc@users.noreply.github.com>
2024-09-25 15:00:29 +0200

    Update data_processor.py
    Moved in 'mask'-related commands

--- DMD/data_processor.py
+++ DMD/data_processor.py
@@ -1,16 +1,19 @@
 import torch as pt
-from data_loader import load_data, apply_mask
-from config import MIN_TIME, FIELD_NAME
+from data_loader import load_data
+from config import DATASET_TYPE, MIN_TIME, FIELD_NAME
+from flowtorch.data import mask_box
 
 def process_data():
-    loader, times, fields, pts = load_data()
+    times, _, pts, loader = load_data()
+    mask = mask_box(pts, lower=MASK_LOWER_BOUND, upper=MASK_UPPER_BOUND)
 
-    # Vortex shedding is complete after 4s, keep only time steps between 4s and 10s
-    t_steps = [t for t in times if float(t) >= MIN_TIME]
+    # In case the large dataset is used, only times greater than 4s are selected
+    # The reason is that vortex shedding is complete after 4 seconds
+    min_time_threshold = max(4.0, float(times[0]))
+    t_steps = [t for t in times if float(t) >= min_time_threshold]
+    
     dt = round(float(t_steps[1]) - float(t_steps[0]), 3)
-
-    mask = apply_mask(pts)
-    data_matrix = pt.zeros((pt.count_nonzero(mask), len(t_steps)), dtype=pt.float32)
+    data_matrix = pt.zeros(pt.count_nonzero(mask), len(t_steps))
 
     for idx, t in enumerate(t_steps):
         snapshots = loader.load_snapshot(FIELD_NAME, t)
@@ -18,4 +21,4 @@ def process_data():
         # Vorticity is defined as the curl of velocity, it has non-zero values only along z-axis
         data_matrix[:, idx] = pt.masked_select(snapshots[:, 2], mask)
             
-    return t_steps, dt, mask, data_matrix, pts
+    return mask, t_steps, dt, data_matrix
