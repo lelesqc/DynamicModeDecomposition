c535b77d4dafed749c5da3322823f7001df26695
Emanuele Nori <162329829+lelesqc@users.noreply.github.com>
2024-10-05 11:02:04 +0200

    Update functions.py
    Added docstring

--- DMD/functions.py
+++ DMD/functions.py
@@ -1,4 +1,19 @@
 def find_optimal_rank(s, thr):
+    """
+    Function that computes the optimal rank by evaluating singular values, based on chosen threshold.
+
+    Parameters:
+        s (torch.Tensor): Tensor containing singular values.
+        thr (float): Chosen threshold to truncate singular values.
+
+    Returns:
+        optimal_rank (int): Computed optimal rank based on singular values and threshold.
+
+    Raises:
+        ValueError: If chosen threshold is negative.
+        ValueError: If chosen threshold is greater than 100.
+        
+    """
     if thr <= 0:
         raise ValueError("Threshold must be positive")
 
