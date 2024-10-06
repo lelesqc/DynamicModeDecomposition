e31106f3fdb1c4731ac1663d7524f28cdfd89a3e
Emanuele Nori <162329829+lelesqc@users.noreply.github.com>
2024-10-06 16:11:50 +0200

    Update simulation.py
    Added path instructions

--- DMD/simulation.py
+++ DMD/simulation.py
@@ -1,10 +1,10 @@
 import logging
 import torch as pt
 from numpy import pi
-from functions import find_optimal_rank
+from DMD.functions import find_optimal_rank
 from flowtorch.analysis import SVD
-from data_loader import load_data
-from data_processor import process_data
+from DMD.data_loader import load_data
+from DMD.data_processor import process_data
 
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 logger = logging.getLogger(__name__)
